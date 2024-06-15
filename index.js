24613de17761bdc95fa51f19bd1d3127b61f19d4
kevin-arock <kevinstandlee2002@gmail.com>
2024-06-15 23:06:35 +0530

    Make add list and select multiple lists interactive

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-const checks = document.querySelector('input[type="checkbox"]')
+
+const taskInput = document.querySelector('.task');
+const container = document.querySelector('.container');
+const tasks = ["gym","grocery"];
+let checks;
+let lastChecked;
+
+function checkList(e){
+    console.log("hii");
+    let inbt = false;
+    if(e.shiftKey && this.checked){
+        checks.forEach((ck)=>{
+            if(ck === this || ck === lastChecked){
+                inbt = !inbt;
+            }
+
+            if(inbt){
+                console.log(ck);
+                ck.checked = true;
+            }
+        })
+    }
+    lastChecked = this;
+}
+
+
+
+function addTask(){
+    tasks.push(taskInput.value);
+    display();
+   
+}
+
+
+function display(){
+    let html = ``;
+    tasks.forEach((task)=>{
+        html+=`<div class="item">
+                    <input type="checkbox">
+                    <p>${task}</p>
+                </div>`
+        ;
+    });
+
+    container.innerHTML = html;
+    checks = document.querySelectorAll('.container input[type="checkbox"]');
+    checks.forEach((ck)=>{
+        ck.addEventListener('click',checkList);
+    });
+    console.log(checks);
+}
+display();
+
+
